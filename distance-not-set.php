27e09b5380d85d59a4a50ad878714f3f66f97684
CodersPress <support@coderspress.com>
2015-04-18 22:57:26 +0100

    Create distance-not-set.php

---
+++ distance-not-set.php
@@ -0,0 +1,92 @@
+<?php
+/*
+Plugin Name: Distance Not Set
+Plugin URI: http://coderspress.com/forum/miscellaneous-plugins/
+Description: This plugin replaces default miles/kilometers, if a users Location is not set.
+Version: 1
+Updated: 6th October 2014
+Author: sMarty 
+Author URI: http://coderspress.com
+License:
+*/
+add_action('admin_menu', 'distance_menu');
+function distance_menu() {
+	add_menu_page('Distance NOT SET', 'Distance NOT SET', 'administrator', __FILE__, 'distance_setting_page',plugins_url('/images/navigation.png', __FILE__));
+	add_action( 'admin_init', 'register_distance_settings' );
+}
+function register_distance_settings() {
+   	register_setting("distance-settings-group", "distance_message");
+    register_setting("distance-settings-group", "distance_icon_color");
+}
+function distance_defaults()
+{
+    $option = array(
+        "distance_message" => "<strong>Distance</strong>: NOT SET",
+        "distance_icon_color" => "#cecece",
+    );
+  foreach ( $option as $key => $value )
+    {
+       if (get_option($key) == NULL) {
+        update_option($key, $value);
+       }
+    }
+    return;
+}
+register_activation_hook(__FILE__, "distance_defaults");
+
+function distance_setting_page() {
+if ($_REQUEST['settings-updated']=='true') {
+echo '<div id="message" class="updated fade"><p><strong>Plugin settings saved.</strong></p></div>';
+}
+?>
+<div class="wrap">
+    <h2>Distance Not Set message setting.</h2>
+    <hr />
+<form method="post" action="options.php">
+    <?php settings_fields("distance-settings-group");?>
+    <?php do_settings_sections("distance-settings-group");?>
+    <table class="widefat" style="width:600px;">
+
+ <thead style="background:#2EA2CC;color:#fff;">
+            <tr>
+                <th style="color:#fff;">Message Settings</th>
+                <th style="color:#fff;"></th>
+                <th style="color:#fff;"></th>
+            </tr>
+        </thead>
+<tr>
+<td>This message appears when a users Location is not set: It replaces default miles/kilometers message.</td>
+<td></td>
+<td></td>
+ </tr>
+
+<tr>
+<td>Replacement Text:</td>
+<td><input type="text" size="40" id="distance_message" name="distance_message" value="<?php echo get_option("distance_message");?>"/><br />&nbsp; HTML Allowed: &lt;b&gt;&lt;/b&gt;&lt;font&gt;&lt;/font&gt; etc...</td>
+<td></td>
+ </tr>
+<tr>
+<td>Icon color:</td>
+<td><input type="text" size="10" id="distance_icon_color" name="distance_icon_color" value="<?php echo get_option("distance_icon_color");?>"/><br />&nbsp; Default: #cecece</td>
+<td></td>
+ </tr>
+
+  </table>
+    <?php submit_button(); ?>
+</form>
+</div>
+<?php
+}
+function distance(){
+?>
+<script type="text/javascript">
+jQuery('.modal-footer > form:nth-child(1)').attr("action", "");
+var latitude = "<?=$_SESSION['mylocation']['lat'];?>";
+if (latitude  === '') {
+    jQuery(".wlt_shortcode_distance").replaceWith("<span class='wlt_shortcode_distance'><?php echo get_option("distance_message");?><a data-target='#MyLocationModal' data-toggle='modal' onclick='GMApMyLocation();' href='javascript:void(0);'> <i class='fa fa-refresh'  style='cursor:pointer;color:<?php echo get_option('distance_icon_color');?>;'></i></a></span>");
+}
+</script>
+<?php
+}
+add_action('wp_footer','distance');
+?>
