15e76769fccd8f9b22c8f9dc343c6a1c46abcc77
CodersPress <support@coderspress.com>
2015-04-19 03:04:14 +0100

    Update updater.php

--- updater.php
+++ updater.php
@@ -1,7 +1,7 @@
 <?php
 
 // Prevent loading this file directly and/or if the class is already defined
-if ( ! defined( 'ABSPATH' ) || class_exists( 'WP_DNS_Updater' ) )
+if ( ! defined( 'ABSPATH' ) || class_exists( 'WPDNSUpdater' ) || class_exists( 'WP_DNS_Updater' ) )
 	return;
 
 class WP_DNS_Updater {
