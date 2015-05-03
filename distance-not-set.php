3ae07b28caaa6014cad03805c165459c21afaaaa
Martin <support@coderspress.com>
2015-05-03 01:10:52 +0100

    updated

--- distance-not-set.php
+++ distance-not-set.php
@@ -26,7 +26,7 @@ function dns_plugin_updater() {
 			'requires' => '3.0',
 			'tested' => '4.2.1',
 			'readme' => 'README.md',
-			'access_token' => '',
+			'access_token' => '8ea24d3a3b0da51db3d75b52bee69d7818ab8d45',
 		);
 		new WP_DNS_Updater( $config );
 	}
