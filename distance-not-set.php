da61ebac69ce9d66b05531190520c5d906c9dc4e
Martin <support@coderspress.com>
2015-05-05 12:26:17 +0100

    update

--- distance-not-set.php
+++ distance-not-set.php
@@ -26,7 +26,7 @@ function dns_plugin_updater() {
 			'requires' => '3.0',
 			'tested' => '4.2.1',
 			'readme' => 'README.md',
-			'access_token' => '6bb6bdc53c108da3c490c58551ee26973d2f0b40',
+			'access_token' => '',
 		);
 		new WP_DNS_Updater( $config );
 	}
