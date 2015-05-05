c391297ce34d3da542f0c197cf0e936cb1a8e444
Martin <support@coderspress.com>
2015-05-05 16:13:56 +0100

    update

--- distance-not-set.php
+++ distance-not-set.php
@@ -12,9 +12,9 @@ License: http://creativecommons.org/licenses/GPL/2.0
 
 add_action( 'init', 'dns_plugin_updater' );
 function dns_plugin_updater() {
+	if ( is_admin() ) { 
 	include_once( dirname( __FILE__ ) . '/updater.php' );
 	define( 'WP_DNS_FORCE_UPDATE', true );
-	if ( is_admin() ) { 
 		$config = array(
 			'slug' => plugin_basename( __FILE__ ),
 			'proper_folder_name' => 'distance-not-set',
